01b87facc65c9f3b5865a8a915c1b68426552949
sayah.faycal <sayah.faycal@protonmail.com>
2021-11-24 18:40:26 +0000

    added strip_command.py

---
+++ stdin_processor/strip_command.py
@@ -0,0 +1,50 @@
+import typer
+from typing import List
+from stdin_processor.processor import STDIN
+from stdin_processor import global_args
+
+
+def _strip(line, **kwargs):
+    left = kwargs.get('left', True)
+    right = kwargs.get('right', True)
+
+    l = line
+    if left: l = l.lstrip()
+    if right: l = l.rstrip()
+
+    return l
+
+
+def strip(left: bool = typer.Option(True, '--left/--no-left', '-l/--nl', help='Strip left'),
+          right: bool = typer.Option(True, '--right/--no-right', '-r/--nr', help='Strip right'),
+
+           ____________________________: str = global_args.args_separator,
+           separators: List[str] = global_args.separators,
+           group_by: int = global_args.group_by,
+           group_join: str = global_args.group_join,
+           join: str = global_args.join,
+           unique: bool = global_args.unique,
+           sort: str = global_args.sort,
+           keep: bool = global_args.keep,
+           where: str = global_args.where,
+           indexes: str = global_args.index,
+           _not: bool = global_args._not,
+           ignore_case: bool = global_args.ignore_case
+           ):
+
+
+    stdin = STDIN()
+    stdin.process(lambda x: _strip(x, left=left, right=right),
+                  separators=separators,
+                  group_by=group_by,
+                  group_join=group_join,
+                  unique=unique,
+                  sort=sort,
+                  keep=keep,
+                  where=where,
+                  _not=_not,
+                  ignore_case=ignore_case,
+                  indexes=indexes,
+                  joiner=join)
+
+    print(stdin.value, end='\n' if '\n' in separators else '')
