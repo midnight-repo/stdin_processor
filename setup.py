8d3cb7ff238641ffddc523ee88952533c2ab8ee4
sayah.faycal <sayah.faycal@protonmail.com>
2021-12-01 11:24:07 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 
 setup(name='stdin_processor',
-      version='0.0.52',
+      version='0.0.54',
       description='String and Standard input manipulation CLI',
       author='midnight_repo',
       author_email='midnight_repo@protonmail.com',
