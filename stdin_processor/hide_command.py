7f87c81540e5e868c74cc987fd02e95fa0370b98
sayah.faycal <sayah.faycal@protonmail.com>
2021-12-03 12:21:29 +0000

    fixed hide issue after updating --where --index system

--- stdin_processor/hide_command.py
+++ stdin_processor/hide_command.py
@@ -2,7 +2,8 @@ import typer
 from typing import List
 from stdin_processor.processor import STDIN
 from stdin_processor import global_args
-
+import random
+import string as chars
 
 def hide(
         ______________: str = global_args.args_separator,
@@ -14,28 +15,46 @@ def hide(
         unique: bool = global_args.unique,
         sort: str = global_args.sort,
         sort_key: str = global_args.sort_key,
-        keep: bool = typer.Option(False, '--keep/--no-keep', '-k/--nk', help='Keep the elements that that did not match --where or --index'),
+        keep: bool = global_args.keep,
         where: List[str] = global_args.where,
         indexes: str = global_args.index,
-        _not: bool = typer.Option(True, '--not', '-n', help='What is matched is not and what is not is matched. Use with --where or --index'),
+        _not: bool = global_args._not,
         ignore_case: bool = global_args.ignore_case
 ):
 
 
     stdin = STDIN()
-    stdin.process(lambda x: x,
-                  separators=separators,
-                  clean=clean,
-                  group_by=group_by,
-                  group_join=group_join,
-                  unique=unique,
-                  sort=sort,
-                  sort_key=sort_key,
-                  keep=keep,  # True MAKES NO SENSE IN SHOW COMMAND
-                  where=where,
-                  _not=_not, # difference with show
-                  ignore_case=ignore_case,
-                  indexes=indexes,
-                  joiner=join)
+    # stdin.process(lambda x: x,
+    #               separators=separators,
+    #               clean=clean,
+    #               group_by=group_by,
+    #               group_join=group_join,
+    #               unique=unique,
+    #               sort=sort,
+    #               sort_key=sort_key,
+    #               keep=keep,  # True MAKES NO SENSE IN SHOW COMMAND
+    #               where=where,
+    #               _not=_not, # difference with show
+    #               ignore_case=ignore_case,
+    #               indexes=indexes,
+    #               joiner=join)
+
+
+    # THIS IS JUST A WORKAROUND, TEMPORARY FIX
+    # if match, append random_val to list, before printing, remove random_val from list
+    # if we append '' instead of random_val, it will interfere with --clean option
+    random_val = '!_-<{[(==!-%s-!==)]}>-_!' % ''.join([random.choice(chars.ascii_letters + chars.digits) for i in range(64)])
+    stdin.split(*separators, clean=clean)
+    if group_by > 1:
+        stdin.group_by(group_by, group_join)
+    stdin.match(*where, ignore_case=ignore_case, index_pattern=indexes, keep=keep, _not=_not)
+    stdin.map(lambda x: random_val)
+    while random_val in stdin.value:
+        stdin.value.remove(random_val)
+    if unique:
+        stdin.remove_duplicates()
+    if sort != 'False':
+        stdin.sort(sort, sort_key=sort_key)
+    stdin.join(join)
 
     print(stdin.value, end='' if stdin.value.endswith('\n') else '\n')
